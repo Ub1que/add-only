37a99ba4450f88138d1fc0592d25f8e1b8777382
Ivan Ladyha <evan.ladyha@gmail.com>
2021-12-23 16:10:41 +0300

    prevent removing codelenses on parse error

--- src/providers/OnlyCodeLensProvider.ts
+++ src/providers/OnlyCodeLensProvider.ts
@@ -4,6 +4,8 @@ import AddCodeLens from "../codelens/AddCodeLens";
 import RemoveCodeLens from "../codelens/RemoveCodeLens";
 import getLocations from "../codeParser/codeParser";
 
+let locationsHistory;
+
 export default class OnlyCodeLensProvider implements CodeLensProvider {
   public provideCodeLenses(
     document: TextDocument
@@ -11,33 +13,43 @@ export default class OnlyCodeLensProvider implements CodeLensProvider {
     const createRangeForCodeLens = ({ line }) =>
       document.lineAt(line - 1).range;
 
-    return getLocations(document.getText()).map(
-      ({location, type}) => {
-        if (type === 'add'){
-          return new AddCodeLens(
-            createRangeForCodeLens(location.end),
-            new Range(
-              location.start.line - 1,
-              location.start.column,
-              location.end.line - 1,
-              location.end.column
-            )
-          )
-        }
+      
+    try {
+      const locations = getLocations(document.getText());
 
-        if (type === 'remove'){
-          return new RemoveCodeLens(
-            createRangeForCodeLens(location.end),
-            new Range(
-              location.start.line - 1,
-              location.start.column - 1,
-              location.end.line - 1,
-              location.end.column
+      locationsHistory = locations.map(
+        ({location, type}) => {
+          if (type === 'add'){
+            return new AddCodeLens(
+              createRangeForCodeLens(location.end),
+              new Range(
+                location.start.line - 1,
+                location.start.column,
+                location.end.line - 1,
+                location.end.column
+              )
             )
-          )
+          }
+  
+          if (type === 'remove'){
+            return new RemoveCodeLens(
+              createRangeForCodeLens(location.end),
+              new Range(
+                location.start.line - 1,
+                location.start.column - 1,
+                location.end.line - 1,
+                location.end.column
+              )
+            )
+          }
         }
-      }
-    )
+      )
+
+      return locationsHistory;
+    } catch (error) {
+      return locationsHistory;
+    }
+
   }
 
   public resolveCodeLens?(): CodeLens | Thenable<CodeLens> {
